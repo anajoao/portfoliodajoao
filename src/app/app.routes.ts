d1705eee45731bbc3d4a612d539ffd24774d017a
Ana João <anoliveira4@gmail.com>
2024-11-14 14:47:21 +0000

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { StylingComponent } from './core/styling/styling.component';
+import { PhotographyComponent } from './core/photography/photography.component';
+import { ContactsComponent } from './core/contacts/contacts.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StylingComponent },
+    { path: 'photography', component: PhotographyComponent },
+    { path: 'contacts', component: ContactsComponent },
+    { path : 'styling', redirectTo: '' },
+    { path: '**', component: NotfoundComponent }
+];
